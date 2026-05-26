07a5403242375a934709eec202c1a5e24e2ec77a
Oscar679 <oscarekberg@hotmail.com>
2026-05-26 16:19:51 +0200

    Refactor ProjectileManager: Update explosion graphic handling and enhance tile destruction logic
    - Fixed explosion graphic playback by referencing the correct variable.
    - Introduced a new mapping for broken tiles to replace specific tile values upon destruction.
    - Simplified the tile destruction logic by removing unnecessary checks and comments.
    - Ensured that only grenades trigger radius damage calculations during projectile updates.

--- src/scene/game/ProjectileManager.js
+++ src/scene/game/ProjectileManager.js
@@ -62,7 +62,7 @@ TerraTactics.scene.ProjectileManager.prototype.m_applyExplosion = function (x, y
     this.m_explosionTimer = this.m_gameScene.timers.create({
         duration: 600,
         onStart: function () {
-            this.m_explosionGraphic.animation.play("idle");
+            explosionGraphic.animation.play("idle");
         },
         onComplete: function () {
             explosionGraphic.y = -50;
@@ -99,6 +99,23 @@ TerraTactics.scene.ProjectileManager.prototype.m_knockback = function (player, s
 TerraTactics.scene.ProjectileManager.prototype.m_destroyTileAtHitbox = function (hitbox) {
     var indexes = this.m_gameScene.stage.m_map.front.getTileIndexesInRect(hitbox);
 
+    var brokenTiles = {
+        25: true,
+        26: true,
+        33: true,
+        34: true,
+        49: true,
+        50: true,
+        51: true,
+        52: true,
+        53: true,
+        54: true,
+        55: true,
+        56: true,
+        57: true,
+        58: true
+    };
+
     for (var i = 0; i < indexes.length; i++) {
         var index = indexes[i];
         var width = this.m_gameScene.stage.m_map.widthInTiles;
@@ -109,27 +126,124 @@ TerraTactics.scene.ProjectileManager.prototype.m_destroyTileAtHitbox = function 
             continue;
         }
 
-        // if its already a destroyed edge or a small island, apply empty tile
-        if (value === 13 || value === 14 || value === 8) {
-            this.m_gameScene.stage.m_map.front.setTileValueAt(index, 0);
+        /*
+        if (brokenTiles[value]) {
+            if (column > 0 &&
+                this.m_gameScene.stage.m_map.front.getTileValueAt(index - 1) === 0 && column < width - 1 &&
+                this.m_gameScene.stage.m_map.front.getTileValueAt(index + 1) === 0) {
+                this.m_gameScene.stage.m_map.front.setTileValueAt(index, 0);
+                return;
+            }
+            if (column > 0 && this.m_gameScene.stage.m_map.front.getTileValueAt(index - 1) === 0) {
+                //apply broken island where its empty on the left side of the island
+                this.m_gameScene.stage.m_map.front.setTileValueAt(index, 13);
+            } else if (column < width - 1 && this.m_gameScene.stage.m_map.front.getTileValueAt(index + 1) === 0) {
+                //apply broken island where its empty on the right side of the island
+                this.m_gameScene.stage.m_map.front.setTileValueAt(index, 14);
+            } else {
+                this.m_gameScene.stage.m_map.front.setTileValueAt(index, 0);
+            }
             return;
+        }*/
+
+        // sätt in en söndrig tile för den specifika tilen
+        switch (value) {
+            case 1:
+                this.m_gameScene.stage.m_map.front.setTileValueAt(index, 50);
+                return;
+            case 2:
+                this.m_gameScene.stage.m_map.front.setTileValueAt(index, 50);
+                return;
+            case 3:
+                this.m_gameScene.stage.m_map.front.setTileValueAt(index, 25);
+                return;
+            case 4:
+                this.m_gameScene.stage.m_map.front.setTileValueAt(index, 33);
+                return;
+            case 5:
+                this.m_gameScene.stage.m_map.front.setTileValueAt(index, 49);
+                return;
+            case 6:
+                this.m_gameScene.stage.m_map.front.setTileValueAt(index, 34);
+                return;
+            case 9:
+                this.m_gameScene.stage.m_map.front.setTileValueAt(index, 52);
+                return;
+            case 10:
+                this.m_gameScene.stage.m_map.front.setTileValueAt(index, 52);
+                return;
+            case 11:
+                this.m_gameScene.stage.m_map.front.setTileValueAt(index, 52);
+                return;
+            case 17:
+                this.m_gameScene.stage.m_map.front.setTileValueAt(index, 52);
+                return;
+            case 18:
+                this.m_gameScene.stage.m_map.front.setTileValueAt(index, 52);
+                return;
+            case 19:
+                this.m_gameScene.stage.m_map.front.setTileValueAt(index, 58);
+                return;
+            case 28:
+                this.m_gameScene.stage.m_map.front.setTileValueAt(index, 53);
+                return;
+            case 29:
+                this.m_gameScene.stage.m_map.front.setTileValueAt(index, 53);
+                return;
+            case 30:
+                this.m_gameScene.stage.m_map.front.setTileValueAt(index, 53);
+                return;
+            case 32:
+                this.m_gameScene.stage.m_map.front.setTileValueAt(index, 26);
+                return;
+            case 35:
+                this.m_gameScene.stage.m_map.front.setTileValueAt(index, 51);
+                return;
+            case 36:
+                this.m_gameScene.stage.m_map.front.setTileValueAt(index, 54);
+                return;
+            case 37:
+                this.m_gameScene.stage.m_map.front.setTileValueAt(index, 55);
+                return;
+            case 38:
+                this.m_gameScene.stage.m_map.front.setTileValueAt(index, 51);
+                return;
+            case 39:
+                this.m_gameScene.stage.m_map.front.setTileValueAt(index, 54);
+                return;
+            case 40:
+                this.m_gameScene.stage.m_map.front.setTileValueAt(index, 52);
+                return;
+            case 41:
+                this.m_gameScene.stage.m_map.front.setTileValueAt(index, 49);
+                return;
+            case 42:
+                this.m_gameScene.stage.m_map.front.setTileValueAt(index, 49);
+                return;
+            case 43:
+                this.m_gameScene.stage.m_map.front.setTileValueAt(index, 56);
+                return;
+            case 45:
+                this.m_gameScene.stage.m_map.front.setTileValueAt(index, 50);
+                return;
+            case 46:
+                this.m_gameScene.stage.m_map.front.setTileValueAt(index, 50);
+                return;
+            case 47:
+                this.m_gameScene.stage.m_map.front.setTileValueAt(index, 50);
+                return;
+            case 48:
+                this.m_gameScene.stage.m_map.front.setTileValueAt(index, 57);
+                return;
         }
 
-        if (column > 0 && this.m_gameScene.stage.m_map.front.getTileValueAt(index - 1) === 0 && column < width - 1 && this.m_gameScene.stage.m_map.front.getTileValueAt(index + 1) === 0) {
-            this.m_gameScene.stage.m_map.front.setTileValueAt(index, 0);
-            return;
-        }
+        /*  // if its already a destroyed edge or a small island, apply empty tile
+         if (value === 13 || value === 14 || value === 8) {
+             this.m_gameScene.stage.m_map.front.setTileValueAt(index, 0);
+             return;
+         }*/
 
-        if (column > 0 && this.m_gameScene.stage.m_map.front.getTileValueAt(index - 1) === 0) {
-            //apply broken island where its empty on the left side of the island
-            this.m_gameScene.stage.m_map.front.setTileValueAt(index, 13);
-        } else if (column < width - 1 && this.m_gameScene.stage.m_map.front.getTileValueAt(index + 1) === 0) {
-            //apply broken island where its empty on the right side of the island
-            this.m_gameScene.stage.m_map.front.setTileValueAt(index, 14);
-        } else {
-            this.m_gameScene.stage.m_map.front.setTileValueAt(index, 0);
-        }
-        return;
+
     }
 };
 
@@ -156,7 +270,9 @@ TerraTactics.scene.ProjectileManager.prototype.radiusDamage = function (bullet, 
 TerraTactics.scene.ProjectileManager.prototype.update = function (inactivePlayers) {
     if (this.m_bullet !== null) {
         if (this.m_bullet.hitTest(this.m_gameScene.stage.m_map.front)) {
-            this.radiusDamage(this.m_bullet, inactivePlayers);
+            if (this.m_bullet.m_type === "grenade") {
+                this.radiusDamage(this.m_bullet, inactivePlayers);
+            }
             this.m_destroyTileAtHitbox(this.m_bullet.hitbox);
             this.m_bulletHit(this.m_gameScene.m_activePlayer.character.weapon, this.m_bullet);
             return;
